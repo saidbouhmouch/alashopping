cb12a2fa58c21d0434c6dd6b73a8284e8d1bf404
Said Bouhmouch <said.dev.info@gmail.com>
2021-07-02 14:55:28 +0100

    add UserController

---
+++ app/Http/Controllers/Ui/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Ui;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use App\Models\User;
+//use App\Models\Role;
+use App\Models\Addresse;
+use Illuminate\Support\Facades\DB;
+//use Illuminate\Support\Facades\Hash;
+//use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    
+
+    public function getCurrentUser(){
+
+        $user = null;
+       if(Auth::check()){
+            $user = User::findOrFail(Auth::id());
+            $user->addresse = Addresse::with(['country'])->where('user_id',Auth::id())->where('type','shipping')->first();  //'shipping','billing'
+       }
+
+        return (new Response(json_encode(["isConnected"=>($user)?true:false,"user"=>$user]), 200));
+
+    }
+
+    
+    
+}
